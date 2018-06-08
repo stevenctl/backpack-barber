2600e52bfe3cb5e87694b3afecab6e0b562692b1
slandow <steven.landow@ctl.io>
2018-06-08 06:15:30 -0500

    build

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,32 +1,7 @@
 <template>
     <v-app dark>
-        <!--<v-navigation-drawer-->
-                <!--v-model="drawer"-->
-                <!--fixed-->
-                <!--app-->
-        <!--&gt;-->
-            <!--<v-list dense>-->
-                <!--<v-list-tile @click="">-->
-                    <!--<v-list-tile-action>-->
-                        <!--<v-icon>home</v-icon>-->
-                    <!--</v-list-tile-action>-->
-                    <!--<v-list-tile-content>-->
-                        <!--<v-list-tile-title>Home</v-list-tile-title>-->
-                    <!--</v-list-tile-content>-->
-                <!--</v-list-tile>-->
-                <!--<v-list-tile @click="">-->
-                    <!--<v-list-tile-action>-->
-                        <!--<v-icon>contact_mail</v-icon>-->
-                    <!--</v-list-tile-action>-->
-                    <!--<v-list-tile-content>-->
-                        <!--<v-list-tile-title>Contact</v-list-tile-title>-->
-                    <!--</v-list-tile-content>-->
-                <!--</v-list-tile>-->
-            <!--</v-list>-->
-        <!--</v-navigation-drawer>-->
         <v-toolbar color="secondary" dark fixed app>
-            <img height="50%" src="./public/backpack.png">
-            <!--<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>-->
+            <img height="50%" src="public/backpack.png"/>
             <v-toolbar-title>Backpack Barber</v-toolbar-title>
         </v-toolbar>
         <v-content>
